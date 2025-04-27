f988b2cbf5c1d490b428f740c794ded3bbf651c8
RolandTran <rolandmtran@gmail.com>
2025-04-27 16:45:14 -0700

    Add files via upload

---
+++ LA_2024_02_Feb.R
@@ -0,0 +1,18 @@
+# LA_Analytical_Detective
+# https://data.lacity.org/Public-Safety/Crime-Data-from-2020-to-Present/2nrs-mtv8/data_preview
+LA_Analytical_Detective_URL <- "https://data.lacity.org/Public-Safety/Crime-Data-from-2020-to-Present/2nrs-mtv8/data_preview"
+cat(LA_Analytical_Detective_URL)
+
+setwd("D:/Coding_Exercises/R/The_Analytics_Edge/Assignment_1/LA_Analytical_Detective/2024")
+getwd()
+wrkdir <- getwd()
+wrkdir
+list.files()
+
+# P 1.1 Loading the Data
+# reading the file
+LA_2024_02_Feb <- read.csv("2024_02_Feb.csv", stringsAsFactors = FALSE)
+LA_2024_02_Feb
+str(LA_2024_02_Feb) 
+head(LA_2024_02_Feb)
+tail(LA_2024_02_Feb)
