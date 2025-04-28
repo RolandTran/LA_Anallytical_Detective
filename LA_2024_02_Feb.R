ddb4adf57214e3b953bd64bc8f7be6b86823c597
RolandTran <rolandmtran@gmail.com>
2025-04-27 17:56:03 -0700

    Add files via upload

--- LA_2024_02_Feb.R
+++ LA_2024_02_Feb.R
@@ -16,3 +16,27 @@ LA_2024_02_Feb
 str(LA_2024_02_Feb) 
 head(LA_2024_02_Feb)
 tail(LA_2024_02_Feb)
+summary(LA_2024_02_Feb)
+
+# e.1 How many rows of data (observations) are in this dataset?
+num_obs <- nrow(LA_2024_02_Feb) #1.1 to see number of row
+num_obs
+cat("There are", num_obs, "rows of data (observations) in the dataset.")
+
+
+# 1.2 how many variables are there in this dataset?
+num_var <- ncol(LA_2024_02_Feb)
+num_var
+cat("There are", num_var, "variables in this dataset.")
+
+cat(names(LA_2024_02_Feb), sep = "\n")
+sum(LA_2024_02_Feb$AREA.NAME == "Hollywood")
+max(LA_2024_02_Feb$Crm.Cd.Desc)
+sum(LA_2024_02_Feb$Crm.Cd.Desc == "VIOLATION OF TEMPORARY RESTRAINING ORDER")
+length(unique(LA_2024_02_Feb$Crm.Cd.Desc))
+unique(LA_2024_02_Feb$Crm.Cd.Desc)
+unique(LA_2024_02_Feb$Premis.Desc)
+length(unique(LA_2024_02_Feb$Premis.Desc))
+max((LA_2024_02_Feb$Premis.Desc))
+sum(LA_2024_02_Feb$Premis.Desc == "YARD (RESIDENTIAL/BUSINESS)")
+    
